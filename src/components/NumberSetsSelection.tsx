07a4f40ff26c59d5d771f444ccf5021427e5c068
micaelabar <148156209+micaelabar@users.noreply.github.com>
2024-11-06 13:56:16 -0500

    Update NumberSetsSelection.tsx

--- src/components/NumberSetsSelection.tsx
+++ src/components/NumberSetsSelection.tsx
@@ -1,2 +1,33 @@
+import React from 'react';
 
+interface NumberSetsSelectionProps {
+    numberSets: number[][];
+    selectedSetIndex: number | null;
+    handleSelectSet: (index: number) => void;
+}
+
+const NumberSetsSelection: React.FC<NumberSetsSelectionProps> = ({
+    numberSets,
+    selectedSetIndex,
+    handleSelectSet,
+}) => {
+    return (
+        <div>
+            {numberSets.map((set, index) => (
+                <button
+                    key={index}
+                    className="boton-transparente" /* Aplica la clase aquí */
+                    onClick={() => handleSelectSet(index)}
+                    style={{
+                        backgroundColor: selectedSetIndex === index ? 'lightgreen' : 'transparent',
+                    }}
+                >
+                    Conjunto {index + 1}: {set.join(', ')}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default NumberSetsSelection;
 
